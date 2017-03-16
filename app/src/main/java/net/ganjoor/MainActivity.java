b529cf5194c4c372060e2091fb428a05f08d009a
‌Behrouz Khezry <bkhezry@gmail.com>
2017-03-17 00:36:31 +0330

    change bottom sheet items. remove two icon.

--- app/src/main/java/net/ganjoor/MainActivity.java
+++ app/src/main/java/net/ganjoor/MainActivity.java
@@ -17,14 +17,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_bookmark:
+                    mTextMessage.setText(R.string.title_bookmark);
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
                     return true;
             }
             return false;
