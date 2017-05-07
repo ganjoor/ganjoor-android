5021f43ea413914bcc84f4419e607fea1dce401e
Behrouz Khezry <bkhezry@gmail.com>
2017-05-08 00:54:13 +0430

    add getContext function to Application class. use static context in AppUtils.

--- app/src/main/java/net/ganjoor/MyApplication.java
+++ app/src/main/java/net/ganjoor/MyApplication.java
@@ -1,16 +1,21 @@
 package net.ganjoor;
 
 import android.app.Application;
+import android.content.Context;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class MyApplication extends Application {
-
+    private static Context context;
     @Override
     public void onCreate() {
         super.onCreate();
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/DroidNaskh-Regular.ttf")
                 .setFontAttrId(R.attr.fontPath).build());
+        MyApplication.context = getApplicationContext();
+    }
+    public static Context getAppContext() {
+        return MyApplication.context;
     }
 }
