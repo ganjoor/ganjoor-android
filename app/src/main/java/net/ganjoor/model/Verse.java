d7a4be6a1493578553e89c84efc78d30343a6d6e
Behrouz Khezry <bkhezry@gmail.com>
2017-05-09 00:02:18 +0430

    replace Verse adapter with VerseCombine class. remove recycler item click listener class.

--- app/src/main/java/net/ganjoor/model/Verse.java
+++ app/src/main/java/net/ganjoor/model/Verse.java
@@ -1,25 +1,9 @@
 package net.ganjoor.model;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.TextView;
-
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import com.mikepenz.fastadapter.FastAdapter;
-import com.mikepenz.fastadapter.items.AbstractItem;
-import com.mikepenz.fastadapter.listeners.ClickEventHook;
 
-import net.ganjoor.R;
-
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class Verse  extends AbstractItem<Verse, Verse.MyViewHolder> {
+public class Verse {
     @SerializedName("id")
     @Expose
     private String id;
@@ -74,62 +58,5 @@ public class Verse  extends AbstractItem<Verse, Verse.MyViewHolder> {
 
     public void setText(String text) {
         this.text = text;
-    }
-    @Override
-    public MyViewHolder getViewHolder(View v) {
-        return new MyViewHolder(v);
-    }
-
-    @Override
-    public int getType() {
-        return R.id.fastadapter_sampleitem_id;
-    }
-
-    @Override
-    public int getLayoutRes() {
-        return R.layout.verse_card;
-    }
-
-    //The logic to bind your data to the view
-    @Override
-    public void bindView(MyViewHolder viewHolder, List<Object> payloads) {
-        //call super so the selection is already handled for you
-        super.bindView(viewHolder, payloads);
-        viewHolder.checkBox.setChecked(isSelected());
-        viewHolder.verse_text.setText(text);
-    }
-
-    //reset the view here (this is an optional method, but recommended)
-    @Override
-    public void unbindView(MyViewHolder holder) {
-        super.unbindView(holder);
-        holder.verse_text.setText(null);
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.verse_text)
-        TextView verse_text;
-        @BindView(R.id.checkbox)
-        CheckBox checkBox;
-
-        private MyViewHolder(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
-        }
-    }
-
-    public static class CheckBoxClickEvent extends ClickEventHook<Verse> {
-        @Override
-        public View onBind(@NonNull RecyclerView.ViewHolder viewHolder) {
-            if (viewHolder instanceof Verse.MyViewHolder) {
-                return ((Verse.MyViewHolder) viewHolder).checkBox;
-            }
-            return null;
-        }
-
-        @Override
-        public void onClick(View v, int position, FastAdapter<Verse> fastAdapter, Verse item) {
-            fastAdapter.toggleSelection(position);
-        }
     }
 }
